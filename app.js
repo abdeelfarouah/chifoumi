3803f11c381726abe284a5a1fd2d6281b00519a8
Abderrahmane El farouah <103990456+Abdeelf902@users.noreply.github.com>
2023-03-30 15:37:15 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,79 +1,54 @@
-// Sélection des éléments HTML
-const buttons = document.querySelectorAll('.choices button');
-const score = document.querySelector('.score-board');
-const result = document.querySelector('.resultat');
-const userScore = document.querySelector('#user-label');
-const computerScore = document.querySelector('#computer-label');
+// Récupérer les éléments de l'interface utilisateur
+const rockBtn = document.getElementById("rock");
+const paperBtn = document.getElementById("paper");
+const scissorsBtn = document.getElementById("scissors");
+const resultDiv = document.getElementById("result");
+const scoresTable = document.getElementById("scores");
 
-// Initialisation des scores
-let userScoreCount = 0;
-let computerScoreCount = 0;
+// Initialiser les scores des joueurs
+let userScore = 0;
+let computerScore = 0;
 
-// Fonction pour l'ordinateur choisissant aléatoirement Pierre, Papier ou Ciseaux
-function computerPlay() {
-  const choices = ['rock', 'paper', 'scissors'];
-  const randomIndex = Math.floor(Math.random() * 3);
-  return choices[randomIndex];
-}
-
-// Fonction pour mettre à jour le score affiché
-function updateScore() {
-  userScore.textContent = `ME: ${userScoreCount} |`;
-  computerScore.textContent = ` COMPUTER: ${computerScoreCount}`;
-}
-
-// Fonction qui compare le choix du joueur et celui de l'ordinateur pour déterminer le gagnant
-function playRound(playerSelection) {
-  const computerSelection = computerPlay();
-  if (playerSelection === computerSelection) {
-    result.textContent = "It's a tie!";
-    return;
-  }
-  if (playerSelection === 'rock') {
-    if (computerSelection === 'scissors') {
-      userScoreCount++;
-      result.textContent = 'You win! Rock beats scissors.';
-      updateScore();
-      return;
-    } else {
-      computerScoreCount++;
-      result.textContent = 'You lose! Paper beats rock.';
-      updateScore();
-      return;
-    }
-  }
-  if (playerSelection === 'paper') {
-    if (computerSelection === 'rock') {
-      userScoreCount++;
-      result.textContent = 'You win! Paper beats rock.';
-      updateScore();
-      return;
-    } else {
-      computerScoreCount++;
-      result.textContent = 'You lose! Scissors beats paper.';
-      updateScore();
-      return;
-    }
-  }
-  if (playerSelection === 'scissors') {
-    if (computerSelection === 'paper') {
-      userScoreCount++;
-      result.textContent = 'You win! Scissors beats paper.';
-      updateScore();
-      return;
-    } else {
-      computerScoreCount++;
-      result.textContent = 'You lose! Rock beats scissors.';
-      updateScore();
-      return;
-    }
-  }
-}
-
-// Écoute des clics sur les boutons de choix
-buttons.forEach(button => {
-  button.addEventListener('click', () => {
-    const playerSelection = button.id;
-    playRound(playerSelection);
-  });
+// Ajouter des écouteurs d'événements sur les boutons de choix
+rockBtn.addEventListener("click", function() {
+  playRound("rock");
 });
+
+paperBtn.addEventListener("click", function() {
+  playRound("paper");
+});
+
+scissorsBtn.addEventListener("click", function() {
+  playRound("scissors");
+});
+
+// Fonction pour jouer un tour de jeu
+function playRound(playerSelection) {
+  // Générer une sélection aléatoire pour l'ordinateur
+  const options = ["rock", "paper", "scissors"];
+  const computerSelection = options[Math.floor(Math.random() * 3)];
+  
+  // Vérifier qui a gagné ou s'il y a une égalité
+  if (playerSelection === computerSelection) {
+    resultDiv.textContent = "Égalité!";
+  } else if (playerSelection === "rock" && computerSelection === "scissors" ||
+             playerSelection === "paper" && computerSelection === "rock" ||
+             playerSelection === "scissors" && computerSelection === "paper") {
+    userScore++;
+    resultDiv.textContent = "Vous avez gagné!";
+  } else {
+    computerScore++;
+    resultDiv.textContent = "Vous avez perdu!";
+  }
+  
+  // Mettre à jour les scores et le tableau des résultats
+  document.getElementById("user-score").textContent = userScore;
+  document.getElementById("computer-score").textContent = computerScore;
+  const newRow = scoresTable.insertRow(-1);
+  const userCell = newRow.insertCell(0);
+  const computerCell = newRow.insertCell(1);
+  const resultCell = newRow.insertCell(2);
+  userCell.textContent = userScore;
+  computerCell.textContent = computerScore;
+  resultCell.textContent = resultDiv.textContent;
+}
