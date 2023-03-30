c0ef0f197c2b52071126e8c3e37bdefcb17149d4
Abderrahmane El farouah <103990456+Abdeelf902@users.noreply.github.com>
2023-03-30 15:01:31 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,34 +1,79 @@
-const userscore = 0;
-const computerscore = 0;
-const userScore_span = document.getElementById("user-score");
-const computerScore_span = document.getElementById("computer-score");
-const scoreBoard = document.querySelector(".score-board");
-const result = document.querySelector(".resultat");
-const rock = document.getElementById("r");
-const paper = document.getElementById("p");
-const scissors = document.getElementById("s");
+// Sélection des éléments HTML
+const buttons = document.querySelectorAll('.choices button');
+const score = document.querySelector('.score-board');
+const result = document.querySelector('.resultat');
+const userScore = document.querySelector('#user-label');
+const computerScore = document.querySelector('#computer-label');
 
-function getComputerChoice(){
-  const choices = ['r', 'p', 's'];
-  const randomNumber = Math.floor(Math.random()*3);
-  return choices[randomNumber];
+// Initialisation des scores
+let userScoreCount = 0;
+let computerScoreCount = 0;
+
+// Fonction pour l'ordinateur choisissant aléatoirement Pierre, Papier ou Ciseaux
+function computerPlay() {
+  const choices = ['rock', 'paper', 'scissors'];
+  const randomIndex = Math.floor(Math.random() * 3);
+  return choices[randomIndex];
 }
 
-function game(userChoice){
-  const computerChoice =getComputerChoice();
-  console.log(computerChoice);
-
+// Fonction pour mettre à jour le score affiché
+function updateScore() {
+  userScore.textContent = `ME: ${userScoreCount} |`;
+  computerScore.textContent = ` COMPUTER: ${computerScoreCount}`;
 }
 
-function main(){
-rock.addEventListener('click', function(){
-  game("r");
-},
-scissors.addEventListener('click', function(){
-  game("r");
-},
-paper.addEventListener('click', function(){
- game("p");
-})
+// Fonction qui compare le choix du joueur et celui de l'ordinateur pour déterminer le gagnant
+function playRound(playerSelection) {
+  const computerSelection = computerPlay();
+  if (playerSelection === computerSelection) {
+    result.textContent = "It's a tie!";
+    return;
+  }
+  if (playerSelection === 'rock') {
+    if (computerSelection === 'scissors') {
+      userScoreCount++;
+      result.textContent = 'You win! Rock beats scissors.';
+      updateScore();
+      return;
+    } else {
+      computerScoreCount++;
+      result.textContent = 'You lose! Paper beats rock.';
+      updateScore();
+      return;
+    }
+  }
+  if (playerSelection === 'paper') {
+    if (computerSelection === 'rock') {
+      userScoreCount++;
+      result.textContent = 'You win! Paper beats rock.';
+      updateScore();
+      return;
+    } else {
+      computerScoreCount++;
+      result.textContent = 'You lose! Scissors beats paper.';
+      updateScore();
+      return;
+    }
+  }
+  if (playerSelection === 'scissors') {
+    if (computerSelection === 'paper') {
+      userScoreCount++;
+      result.textContent = 'You win! Scissors beats paper.';
+      updateScore();
+      return;
+    } else {
+      computerScoreCount++;
+      result.textContent = 'You lose! Rock beats scissors.';
+      updateScore();
+      return;
+    }
+  }
 }
-main();
+
+// Écoute des clics sur les boutons de choix
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    const playerSelection = button.id;
+    playRound(playerSelection);
+  });
+});
