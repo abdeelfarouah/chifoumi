8814124576d2088ad82d1cf07fe326c4294724b0
abderrahmane el farouah <abderrahmane.elfarouah@laposte.net>
2023-04-13 17:57:58 +0200

    work in progress

--- app.js
+++ app.js
@@ -1,127 +1,75 @@
-// Définition des variables
-
+// Définition des constantes
+const playerScoreElement = document.getElementById('player-score');
+const computerScoreElement = document.getElementById('computer-score');
+const resultMsgElement = document.getElementById('result');
 const choices = document.querySelectorAll('.choices img');
-const userScore = document.getElementById('user-score');
-const computerScore = document.getElementById('computer-score');
-const result = document.getElementById('result');
-const scoresTable = document.getElementById('scores');
-let userChoice;
-let computerChoice;
-let userScoreValue = 0;
-let computerScoreValue = 0;
-let resultText = "";
+const resetBtn = document.querySelector("#reset");
 
-// Fonction de choix aléatoire de l'ordinateur
+// Définition des variables
+let playerScore = 0;
+let computerScore = 0;
 
-function getComputerChoice() {
-  const choices = ['rock', 'paper', 'scissors'];
-  const randomIndex = Math.floor(Math.random() * 3);
-  return choices[randomIndex];
-}
+// Ajouter un écouteur d'événement à chaque bouton
+choices.forEach(choice => {
+  choice.addEventListener('click', handleChoiceClick);
+});
 
-// Fonction de comparaison des choix et mise à jour du score
+// Ajouter un écouteur d'événement au bouton de réinitialisation
+resetBtn.addEventListener("click", () => {
+  playerScore = 0;
+  computerScore = 0;
+  updateScores();
+  resultMsgElement.textContent = "";
+});
 
-function updateScore(userChoice, computerChoice, userScore, computerScore) {
-  let resultText;
-  
-  switch (userChoice + computerChoice) {
-    case 'rockscissors':
-    case 'paperrock':
-    case 'scissorspaper':
-      userScore++;
-      resultText = "You win!";
-      break;
-    case 'rockpaper':
-    case 'paperscissors':
-    case 'scissorsrock':
-      computerScore++;
-      resultText = "You lose!";
-      break;
-    default:
-      resultText = "It's a tie!";
+// Définir la fonction pour gérer les clics sur les images  
+function handleChoiceClick(event) {
+  // Récupérer l'ID de l'image cliquée pour déterminer le choix du joueur
+  const playerChoice = event.target.id;
+
+  // Déterminer le choix de l'ordinateur
+  const computerChoice = getComputerChoice();
+
+  // Déterminer le résultat du jeu
+  const result = getResult(playerChoice, computerChoice);
+
+  // Mettre à jour le score en fonction du résultat
+  if (result === 'player') {
+    playerScore++;
+  } else if (result === 'computer') {
+    computerScore++;
   }
-  
-  return {
-    userScore,
-    computerScore,
-    resultText
-  };
-}
-  userScore.textContent = userScoreValue;
-  computerScore.textContent = computerScoreValue;
 
-  // Ajout d'une ligne au tableau des scores
+  // Afficher le résultat du jeu
+  resultMsgElement.textContent = `You chose ${playerChoice}, computer chose ${computerChoice}. ${result}`;
 
-  const row = scoresTable.insertRow(-1);
-  const userCell = row.insertCell(0);
-  const computerCell = row.insertCell(1);
-  const resultCell = row.insertCell(2);
-  userCell.textContent = userChoice;
-  computerCell.textContent = computerChoice;
-  resultCell.textContent = resultText;
+  // Mettre à jour les scores affichés dans la page HTML
+  updateScores();
 }
 
-// Fonction principale de jeu
-
-function play() {
-  computerChoice = getComputerChoice();
-
-  // Détection du choix de l'utilisateur et mise à jour de l'image
-
-  choices.forEach(choice => {
-    choice.addEventListener('click', function() {
-      userChoice = choice.id;
-      this.classList.add('selected');
-      setTimeout(() => this.classList.remove('selected'), 500);
-      updateScore();
-      result.textContent = resultText;
-    });
-  });play();
+// Définir une fonction pour générer le choix aléatoire de l'ordinateur
+function getComputerChoice() {
+  const choices = ["rock", "paper", "scissors"];
+  return choices[Math.floor(Math.random() * 3)];
 }
-function showScores(userScore, computerScore, resultText) {
-  const table = document.createElement('table');
-  const thead = document.createElement('thead');
-  const tbody = document.createElement('tbody');
-  const trHead = document.createElement('tr');
-  const thMe = document.createElement('th');
-  const thComputer = document.createElement('th');
-  const thResult = document.createElement('th');
-  
-  thMe.textContent = 'ME';
-  thComputer.textContent = 'COMPUTER';
-  thResult.textContent = 'RESULT';
-  
-  trHead.appendChild(thMe);
-  trHead.appendChild(thComputer);
-  trHead.appendChild(thResult);
-  
-  thead.appendChild(trHead);
-  table.appendChild(thead);
-  table.appendChild(tbody);
-  
-  const trBody = document.createElement('tr');
-  const tdUserScore = document.createElement('td');
-  const tdComputerScore = document.createElement('td');
-  const tdResultText = document.createElement('td');
-  
-  tdUserScore.textContent = userScore;
-  tdComputerScore.textContent = computerScore;
-  tdResultText.textContent = resultText;
-  
-  trBody.appendChild(tdUserScore);
-  trBody.appendChild(tdComputerScore);
-  trBody.appendChild(tdResultText);
-  
-  tbody.appendChild(trBody);
-  
-  const popup = window.open('', 'popup', 'width=300,height=200');
-  popup.document.body.appendChild(table);
+
+// Définir une fonction pour déterminer le résultat du jeu
+function getResult(playerChoice, computerChoice) {
+  if (playerChoice === computerChoice) {
+    return "Tie!";
+  } else if (
+    (playerChoice === "rock" && computerChoice === "scissors") ||
+    (playerChoice === "paper" && computerChoice === "rock") ||
+    (playerChoice === "scissors" && computerChoice === "paper")
+  ) {
+    return "You win!";
+  } else {
+    return "Computer wins!";
+  }
 }
-const scores = updateScore(userChoice, computerChoice, userScoreValue, computerScoreValue);
-userScoreValue = scores.userScore;
-computerScoreValue = scores.computerScore;
-resultText = scores.resultText;
 
-showScores(userScoreValue, computerScoreValue, resultText);
-
-
+// Définir une fonction pour mettre à jour les scores affichés dans la page HTML
+function updateScores() {
+  playerScoreElement.textContent = playerScore;
+  computerScoreElement.textContent = computerScore;
+}
