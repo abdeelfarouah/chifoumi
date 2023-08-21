e567245394520486e73443f8a6cd8ecf757a3d56
abderrahmane el farouah <abderrahmane.elfarouah@laposte.net>
2023-08-21 21:22:47 +0200

    fix all

--- app.js
+++ app.js
@@ -2,33 +2,20 @@ document.addEventListener("DOMContentLoaded", () => {
   const playerNameInput = document.getElementById("playerName");
   const startButton = document.getElementById("startButton");
   const resultElement = document.getElementById("result");
-  const playerScoreElement = document.getElementById("playerScore");
-  const computerScoreElement = document.getElementById("computerScore");
   const nameErrorMessage = document.getElementById("nameErrorMessage");
   const scoreTableBody = document.getElementById("scoreBody");
-
-  let playerName = "";
-  let playerScore = 0;
-  let computerScore = 0;
-  let gameCount = 0;
 
   const choices = ["rock", "paper", "scissors"];
   const choiceImages = {
     rock: document.getElementById("rock"),
     paper: document.getElementById("paper"),
-    scissors: document.getElementById("scissors")
+    scissors: document.getElementById("scissors"),
   };
 
-  // Charger les scores et noms depuis le stockage local
-  let playerScores = JSON.parse(localStorage.getItem("playerScores")) || [];
-
-  // Fonction pour sauvegarder les scores dans le stockage local
-  function saveScoresToLocalStorage() {
-    localStorage.setItem("playerScores", JSON.stringify(playerScores));
-  }
+  let players = [];
 
   playerNameInput.addEventListener("input", () => {
-    playerName = playerNameInput.value.trim();
+    const playerName = playerNameInput.value.trim();
     if (playerName === "") {
       startButton.disabled = true;
       nameErrorMessage.style.display = "block";
@@ -39,22 +26,28 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   startButton.addEventListener("click", () => {
+    const playerName = playerNameInput.value.trim();
     if (playerName === "") {
       resultElement.textContent = "Please enter a valid name.";
       return;
     }
 
     resultElement.textContent = "Choose your weapon:";
-    playerScore = 0;
-    computerScore = 0;
-    playerScoreElement.textContent = playerScore;
-    computerScoreElement.textContent = computerScore;
-
+    const currentPlayer = players.find((player) => player.name === playerName);
+    if (!currentPlayer) {
+      const newPlayer = {
+        name: playerName,
+        playerScore: 0,
+        computerScore: 0,
+      };
+      players.push(newPlayer);
+      addNewPlayerRow(newPlayer);
+    }
     enableChoiceButtons();
   });
 
-  function playRound(playerSelection, computerSelection) {
-    if (playerName === "") {
+  function playRound(playerSelection, computerSelection, currentPlayer) {
+    if (!currentPlayer) {
       resultElement.textContent = "Please enter a valid name before playing.";
       return;
     }
@@ -66,114 +59,71 @@ document.addEventListener("DOMContentLoaded", () => {
       (playerSelection === "paper" && computerSelection === "rock") ||
       (playerSelection === "scissors" && computerSelection === "paper")
     ) {
-      playerScore++;
+      currentPlayer.playerScore++;
       return "You win!";
     } else {
-      computerScore++;
+      currentPlayer.computerScore++;
       return "Computer wins!";
     }
   }
 
-  choices.forEach(choice => {
+  choices.forEach((choice) => {
     choiceImages[choice].addEventListener("click", () => {
-      if (playerName === "") {
+      const playerName = playerNameInput.value.trim();
+      const currentPlayer = players.find((player) => player.name === playerName);
+
+      if (!currentPlayer) {
         resultElement.textContent = "Please enter a valid name before playing.";
         return;
       }
 
       const computerChoice = choices[Math.floor(Math.random() * choices.length)];
-      const result = playRound(choice, computerChoice);
-      resultElement.textContent = `${result} ${playerName} chose ${choice} and the computer chose ${computerChoice}.`;
+      const result = playRound(choice, computerChoice, currentPlayer);
+      resultElement.textContent = `${result} ${
+        currentPlayer.name
+      } chose ${choice} and the computer chose ${computerChoice}.`;
 
-      playerScoreElement.textContent = playerScore;
-      computerScoreElement.textContent = computerScore;
-
-      if (playerScore === 10) {
-        alert(`Congratulations ${playerName}! You win the game!`);
-        saveScore(playerName, playerScore, computerScore); // Enregistrer le score
-        resetGame();
-      } else if (computerScore === 10) {
-        alert(`Oops ${playerName}! You lose the game.`);
-        saveScore(playerName, playerScore, computerScore); // Enregistrer le score
-        resetGame();
-      } else {
-        gameCount++;
-        if (gameCount === 10) {
-          alert(`Game over. ${playerName}, you have completed 10 games.`);
-          resetGame();
+      updateScores();
+      if (
+        currentPlayer.playerScore === 10 ||
+        currentPlayer.computerScore === 10
+      ) {
+        if (currentPlayer.playerScore === 10) {
+          alert(`Congratulations ${currentPlayer.name}! You win the game!`);
+        } else {
+          alert(`Oops ${currentPlayer.name}! You lose the game.`);
         }
       }
-
-      shuffleChoiceImages();
     });
   });
 
-  function resetGame() {
-    gameCount = 0; // Réinitialiser le compteur de parties
-    resultElement.textContent = "";
-    enableChoiceButtons();
-  }
-
   function enableChoiceButtons() {
-    choiceImages.rock.disabled = false;
-    choiceImages.paper.disabled = false;
-    choiceImages.scissors.disabled = false;
-  }
-
-  function shuffleChoiceImages() {
-    const shuffledChoices = choices.slice();
-    for (let i = shuffledChoices.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [shuffledChoices[i], shuffledChoices[j]] = [shuffledChoices[j], shuffledChoices[i]];
-    }
-
-    shuffledChoices.forEach((choice, index) => {
-      choiceImages[choice].style.order = index;
+    choices.forEach((choice) => {
+      choiceImages[choice].style.pointerEvents = "auto";
     });
   }
 
-  // Fonction pour ajouter le score au tableau des scores
-  function addScoreToTable(playerName, playerScore, computerScore) {
-    const newRow = document.createElement("tr");
-    const nameCell = document.createElement("td");
-    const playerScoreCell = document.createElement("td");
-    const computerScoreCell = document.createElement("td");
+  function addNewPlayerRow(player) {
+    const newRow = scoreTableBody.insertRow();
+    const nameCell = newRow.insertCell(0);
+    const playerScoreCell = newRow.insertCell(1);
+    const computerScoreCell = newRow.insertCell(2);
 
-    nameCell.textContent = playerName;
-    playerScoreCell.textContent = playerScore;
-    computerScoreCell.textContent = computerScore;
-
-    newRow.appendChild(nameCell);
-    newRow.appendChild(playerScoreCell);
-    newRow.appendChild(computerScoreCell);
-
-    scoreTableBody.appendChild(newRow);
+    nameCell.textContent = player.name;
+    playerScoreCell.textContent = player.playerScore;
+    computerScoreCell.textContent = player.computerScore;
   }
 
-  // Sauvegarder les scores dans le stockage local
-  function saveScoresToLocalStorage() {
-    localStorage.setItem("playerScores", JSON.stringify(playerScores));
-  }
+  function updateScores() {
+    players.forEach((player) => {
+      const playerRow = [...scoreTableBody.rows].find(
+        (row) => row.cells[0].textContent === player.name
+      );
 
-  // Charger les scores depuis le stockage local (s'il existe)
-  function loadScoresFromLocalStorage() {
-    const storedScores = JSON.parse(localStorage.getItem("playerScores"));
-    if (storedScores) {
-      playerScores = storedScores;
-      playerScores.forEach(score => {
-        addScoreToTable(score.name, score.playerScore, score.computerScore);
-      });
-    }
+      if (playerRow) {
+        playerRow.cells[1].textContent = player.playerScore;
+        playerRow.cells[2].textContent = player.computerScore;
+      }
+    });
   }
-
-  // Enregistrer le score actuel
-  function saveScore(playerName, playerScore, computerScore) {
-    playerScores.push({ name: playerName, playerScore: playerScore, computerScore: computerScore });
-    addScoreToTable(playerName, playerScore, computerScore);
-    saveScoresToLocalStorage();
-  }
-
-  // Appeler la fonction pour initialiser le tableau des scores
-  loadScoresFromLocalStorage();
-  initializeScoreTable();
 });
